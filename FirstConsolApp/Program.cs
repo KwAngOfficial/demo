956de8971bc306e6b7aa69731325d43794073cbf
KwAng204 <kieuquang.official@gmail.com>
2022-05-14 13:13:03 +0700

    taouplen

---
+++ FirstConsolApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstConsolApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
